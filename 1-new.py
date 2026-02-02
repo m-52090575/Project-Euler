6e50d80e2c5c7ee40e4ae97218006c032be7c686
Mary <marywang12345@gmail.com>
2026-02-01 22:00:15 -0800

    fixed

--- 1-new.py
+++ 1-new.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
-max = 1000000000
+import math
+
+max = 1000
 
 def sumDivisibleBy(n):
-    p = (max - 1) / n
-    return n * (p * (p + 1)) / 2
+    p = math.floor((max - 1) / n)
+    return math.floor(n * (p * (p + 1)) / 2)
 
 # a or b - (a and b)
 print(sumDivisibleBy(3) + sumDivisibleBy(5) - sumDivisibleBy(15))
